2e10cd68956f8568ac1f551e5e4df6d0013f6fdd
KmK0708 <kample2k@gmail.com>
2023-11-02 09:39:42 +0900

    1102 깃 커밋

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class AVA
+{
+public:
+	void Startfuction();
+
+private:
+
+};
+
+void AVA::Startfuction()
+{
+	int a = 0;
+	cin >> a;
+
+	for (int i = 0; i < 10; i++)
+	{
+		a = i;
+	}
+}
+
+int main()
+{
+	
+}
